29a35442f333d31682b20fa6dd17124eafd64c0f
Tanner Robison <robison.tanner@gmail.com>
2017-10-23 17:24:28 -0600

    Comments to terrain and plants. Package needs to be made.

--- Plants.r
+++ Plants.r
@@ -1,109 +1,131 @@
-setup.plants <- function(repro, survive, comp.mat, names=NULL){ 
-    if(is.null(names)) 
-        names <- letters[seq_along(repro)]
-    if(length(repro) != length(survive)) 
-        stop("Reproduction and survival parameters needed for all species") 
-    if(nrow(comp.mat) != length(repro))
-        stop("Comptetion matrix dimensions should be of equal length to the number of species")
-    repro <- setNames(repro, names) 
-    survive <- setNames(survive, names)
-    colnames(comp.mat) <- names
-    rownames(comp.mat) <- names
-    return(list(repro=repro, survive=survive, comp.mat=comp.mat, names=names))
-}
+#' Generate plants to exist, reproduce, and compete within the terrain
+#'
+#' Ensure that you have the same length of vectors for params repro, survive and names!!
+#' 
+#' @param repro The rate of reproduction of the listed plants. (default: .4,.8)
+#'          Is a vector of length equal to the number of listed plants
+#' @param survive The rate of survival of the listed plants. (default: .8, .65)
+#'          Is a vector of length equal to the number of listed plants
+#' @param names The names of listed plants. (defaul=NULL)
+#'          If no names are provided, plants will be assigned a letter in the alphabet 
+#'          Is a vector of length equal to the number of listed plants
+#' @param timesteps The number of 'turns' that you want the simulation to exectute. (defaul: 50)
+#' @return a plant matrix; empty cells contain only '', waterlogged cells contain Na,  
+#'          and cells containing plants will have the indicated (or assigned) name. 
+#' @examples 
+#' plants <- make.plants(terrain, survive, reproduce, names)
+#' 
+#'
 
-comp.mat <- matrix(c(.7,.3,.3,.7),2,2)
-setup.plants(c(.4,.6), c(.6,.6),comp.mat)
+make.plants <- function(terrain, survive=c(.7,.85), repro=c(.95,.55),names=NULL, timesteps=50){
 
-survive <- function(cell, info){  
-    if(is.na(cell)){
-        cell <- NA
+    comp.mat <- matrix(c(survive, rev(survive)), length(survive),length(survive))
+
+    setup.plants <- function(repro, survive, comp.mat, names){ 
+        if(is.null(names)) 
+            names <- letters[seq_along(repro)]
+        if(length(repro) != length(survive)) 
+            stop("Reproduction and survival parameters needed for all species") 
+        if(nrow(comp.mat) != length(repro))
+            stop("Comptetion matrix dimensions should be of equal length to the number of species")
+        repro <- setNames(repro, names) 
+        survive <- setNames(survive, names)
+        colnames(comp.mat) <- names
+        rownames(comp.mat) <- names
+        return(list(repro=repro, survive=survive, comp.mat=comp.mat, names=names))
     }
-    else{
-        if(cell == '')
-            cell <- ''
-        else if(runif(1) <= info$survive[cell]){ 
-            cell <- cell
-        }else{
-            cell <- ''
+
+    survival <- function(cell, info){  
+        if(is.na(cell)){
+            cell <- NA
         }
-    }
-    return(cell)
-}      
-
-plant.timestep <- function(plants, terrain, info){
-    for(i in 1:nrow(terrain)){
-        for(j in 1: ncol(terrain)){
-            plants[i,j] <- survive(plants[i,j], info)
-        }   
-    }
-    for(i in 1:nrow(terrain)){
-        for(j in 1: ncol(terrain)){
-            plants <- reproduce(i, j, plants, info)
+        else{
+            if(cell == '')
+                cell <- ''
+            else if(runif(1) <= info$survive[cell]){ 
+                cell <- cell
+            }else{
+                cell <- ''
+            }
         }
-    }
-    return(plants)
-}
+        return(cell)
+    }      
 
-run.plant.ecosystem <- function(terrain, comp.mat, timesteps=50, seed.fracs=c(.1, .1), repro=c(.4, .8), survive=c(.8,.65),names=NULL){ 
-    plants <- array('', dim=c(ncol(terrain),nrow(terrain), timesteps))
-    info <- setup.plants(repro, survive, comp.mat, names) 
-    random.x.val <- sample(1:ncol(terrain),length(terrain)*.1)
-    random.y.val <- sample(1:ncol(terrain),length(terrain)*.1)
-    for(i in 1:length(random.x.val)){
-        choose.species <- sample(1:2,1)
-        if(choose.species == 1)
-            plants[random.x.val[i],random.y.val[i],1] <- info$names[[1]]
-        else
-            plants[random.x.val[i],random.y.val[i],1] <- info$names[[2]]
-    }
-    for(i in seq_len(dim(plants)[3])){ 
-        plants[,,i][is.na(terrain)] <- NA
-    }
-    for(i in seq(2,timesteps)){ 
-        plants[,,i] <- plant.timestep(plants[,,i-1], terrain, info)
-    }
-    return(plants)
-}
-run.plant.ecosystem(terrain, comp.mat) 
-
-reproduce <- function(row, col, plants, info){
-    cell <- plants[row,col]
-    possible.locations <- as.matrix(expand.grid(row+c(-1, 0, 1), col+c(-1, 0, 1)))
-    possible.locations <- na.omit(possible.locations)
-    possible.locations <- actually.possible(row,col,possible.locations,plants)
-    if(is.na(cell))
-        cell <- NA
-    else{
-        if(cell == '')
-            cell <- ''
-        else if(runif(1) <= info$repro[cell]){
-            random.draw <- sample(1:nrow(possible.locations),1)
-            new.row <- possible.locations[random.draw,1]
-            new.col <- possible.locations[random.draw,2]
-            if(plants[new.row,new.col] != ''){
-                fight(plants, comp.mat, cell, info)
-            }else
-                plants[new.row,new.col] <- cell
-            return(plants)
-        }else{
-            cell <- ''
+    plant.timestep <- function(plants, terrain, info){
+        for(i in 1:nrow(terrain)){
+            for(j in 1: ncol(terrain)){
+                plants[i,j] <- survival(plants[i,j], info)
+            }   
         }
-    }
-    return(plants)
-}
-
-
-actually.possible <- function(row,col,possible.locations,plants){
-    for(i in nrow(possible.locations):1){
-        if(possible.locations[i,1] < 1 | possible.locations[i,1] > nrow(plants) | possible.locations[i,2] < 1 | possible.locations[i,2] > nrow(plants)){
-            possible.locations <- possible.locations[-i,]
+        for(i in 1:nrow(terrain)){
+            for(j in 1: ncol(terrain)){
+                plants <- reproduce(i, j, plants, info)
+            }
         }
+        return(plants)
     }
-    return(possible.locations)
-}
 
-fight <- function(plants, comp.mat, cell, info){
-    cell <- sample(info$names, 1, prob=comp.mat[1,])
-    return(cell)
+    run.plant.ecosystem <- function(terrain, comp.mat, survive, repro, names, timesteps){ 
+        plants <- array('', dim=c(ncol(terrain),nrow(terrain), timesteps))
+        info <- setup.plants(repro, survive, comp.mat, names) 
+        random.x.val <- sample(1:ncol(terrain),length(terrain)*.1,replace=TRUE)
+        random.y.val <- sample(1:ncol(terrain),length(terrain)*.1,replace=TRUE)
+        for(i in 1:length(random.x.val)){
+            choose.species <- sample(1:2,1)
+            if(choose.species == 1)
+                plants[random.x.val[i],random.y.val[i],1] <- info$names[[1]]
+            else
+                plants[random.x.val[i],random.y.val[i],1] <- info$names[[2]]
+        }
+        for(i in seq_len(dim(plants)[3])){ 
+            plants[,,i][is.na(terrain)] <- NA
+        }
+        for(i in seq(2,timesteps)){ 
+            plants[,,i] <- plant.timestep(plants[,,i-1], terrain, info)
+        }
+        return(plants)
+    } 
+
+    reproduce <- function(row, col, plants, info){
+        cell <- plants[row,col]
+        possible.locations <- as.matrix(expand.grid(row+c(-1, 0, 1), col+c(-1, 0, 1)))
+        possible.locations <- na.omit(possible.locations)
+        possible.locations <- actually.possible(row,col,possible.locations,plants)
+        if(is.na(cell))
+            cell <- NA
+        else{
+            if(cell == '')
+                cell <- ''
+            else if(runif(1) <= info$repro[cell]){
+                random.draw <- sample(1:nrow(possible.locations),1)
+                new.row <- possible.locations[random.draw,1]
+                new.col <- possible.locations[random.draw,2]
+                if(plants[new.row,new.col] != ''){
+                    fight(plants, comp.mat, cell, info)
+                }else
+                    plants[new.row,new.col] <- cell
+                return(plants)
+            }else{
+                cell <- ''
+            }
+        }
+        return(plants)
+    }
+
+    actually.possible <- function(row,col,possible.locations,plants){
+        for(i in nrow(possible.locations):1){
+            if(possible.locations[i,1] < 1 | possible.locations[i,1] > nrow(plants) | possible.locations[i,2] < 1 | possible.locations[i,2] > nrow(plants)){
+                possible.locations <- possible.locations[-i,]
+            }
+        }
+        return(possible.locations)
+    }
+
+    fight <- function(plants, comp.mat, cell, info){
+        cell <- sample(info$names, 1, prob=comp.mat[1,])
+        return(cell)
+    }
+    plant.plot <- run.plant.ecosystem(terrain, comp.mat, survive, repro, names, timesteps)
+    return(plant.plot)
 }
+make.plants(make.terrain(3,15))
--- Terrain.r
+++ Terrain.r
@@ -1,73 +1,85 @@
-make.terrain <- function(n){
-    #length of sides (always odd)
-    #will has n^2+1, but this doesn't always result in odd numbers, 
-    #which makes diamond step hard
-    sides <- (2^n)+1
-    terrain <- matrix(nrow=sides, ncol=sides)
-    rand.corner <- rnorm(4,20,35)
-    terrain[1,1] <- rand.corner[1]
-    terrain[sides,1] <- rand.corner[2]
-    terrain[sides,sides] <- rand.corner[3]
-    terrain[1,sides] <- rand.corner[4]
-    return(terrain)
-}
+#' Make a terrain with lakes
+#'
+#' This is a wrapper around \code{diamond.square.step} and \code{add.water}
+#' @param n Determines the size of the grid. Grid will be 2^n +1 with a default of n=6
+#' @param water a logical to indicate whether terrain lower than 0 should be underwater (default:TRUE)
+#' @param sd the random noise to be added to each step of diamond.square.step
+#' 
+#' @return a terrain matrix; numeric elements indicate height, NAs indicate cells filled with water
+#' 
+#' @examples 
+#' terry <- make.terrain(4, 15)
+#' image(terry)
+#'
+#'
+make.terrain <- function(n=6, water=TRUE, sd=15){
 
-# Define the functions we'll use
-diamond.step <- function(mat,sd){
-    sides <- sqrt(length(mat))
-    corners <- c(mat[1,sides],mat[1,1],mat[sides,sides],mat[sides,1])
-    corner.mean <- mean(corners) + rnorm(1,0,sd)
-    mid.col <- median(1:ncol(mat))
-    mat[mid.col,mid.col] <- corner.mean
-    return(mat)
-}
-square.step <- function(mat,sd){
-    sides <- ncol(mat)
-    row.center <- median(1:sides)
-    
-    top.center <- c(mat[1,1],mat[1,sides], mat[row.center, row.center])
-    mat[1,row.center] <- mean(top.center) + rnorm(1,0,sd)
-   
-    bottom.center <- c(mat[sides,1],mat[sides,sides], mat[row.center, row.center]) 
-    mat[sides, row.center] <- mean(bottom.center) + rnorm(1,0,sd)
-
-    left.center <- c(mat[sides,1],mat[1,1], mat[row.center, row.center]) 
-    mat[row.center, 1] <- mean(left.center) + rnorm(1,0,sd)
-
-    right.center <- c(mat[1,sides],mat[sides,sides], mat[row.center, row.center]) 
-    mat[row.center, sides] <- mean(right.center) + rnorm(1,0,sd)
-    return(mat)
-}
-
-# Put it all together
-diamond.square.step <- function(n,sd){
-    terrain <- make.terrain(n)
-    terrain.size <- ncol(terrain)-1
-    for (i in 2^(n: 1)){
-        spacing <- seq(1, terrain.size, by=i)
-        for(j in spacing){
-            for(k in spacing){
-                #cat(k,":",(k+i), "    ", j,":",(j+i),"\n")
-                terrain[k:(k+i),j:(j+i)] <- diamond.step(terrain[k:(k+i),j:(j+i)],sd)
-                terrain[k:(k+i),j:(j+i)] <- square.step(terrain[k:(k+i),j:(j+i)],sd)
-            }
-        }
-        sd <- sd * .75
+    make.terrain <- function(n){
+        #length of sides (always odd)
+        #will has n^2+1, but this doesn't always result in odd numbers, 
+        #which makes diamond step hard
+        sides <- (2^n)+1
+        terrain <- matrix(nrow=sides, ncol=sides)
+        rand.corner <- rnorm(4,20,35)
+        terrain[1,1] <- rand.corner[1]
+        terrain[sides,1] <- rand.corner[2]
+        terrain[sides,sides] <- rand.corner[3]
+        terrain[1,sides] <- rand.corner[4]
+        return(terrain)
     }
-        return(terrain) 
-}
 
+    diamond.step <- function(mat,sd){
+        sides <- sqrt(length(mat))
+        corners <- c(mat[1,sides],mat[1,1],mat[sides,sides],mat[sides,1])
+        corner.mean <- mean(corners) + rnorm(1,0,sd)
+        mid.col <- median(1:ncol(mat))
+        mat[mid.col,mid.col] <- corner.mean
+        return(mat)
+    }
 
-make.terrain.wrapper <- function(n, sd, water=TRUE){
+    square.step <- function(mat,sd){
+        sides <- ncol(mat)
+        row.center <- median(1:sides)
+        
+        top.center <- c(mat[1,1],mat[1,sides], mat[row.center, row.center])
+        mat[1,row.center] <- mean(top.center) + rnorm(1,0,sd)
     
-    terry <- diamond.square.step(n,sd)
-    
+        bottom.center <- c(mat[sides,1],mat[sides,sides], mat[row.center, row.center]) 
+        mat[sides, row.center] <- mean(bottom.center) + rnorm(1,0,sd)
+
+        left.center <- c(mat[sides,1],mat[1,1], mat[row.center, row.center]) 
+        mat[row.center, 1] <- mean(left.center) + rnorm(1,0,sd)
+
+        right.center <- c(mat[1,sides],mat[sides,sides], mat[row.center, row.center]) 
+        mat[row.center, sides] <- mean(right.center) + rnorm(1,0,sd)
+        return(mat)
+    }
+
+    diamond.square.step <- function(n,sd){
+        terrain <- make.terrain(n)
+        terrain.size <- ncol(terrain)-1
+        for (i in 2^(n: 1)){
+            spacing <- seq(1, terrain.size, by=i)
+            for(j in spacing){
+                for(k in spacing){
+                    terrain[k:(k+i),j:(j+i)] <- diamond.step(terrain[k:(k+i),j:(j+i)],sd)
+                    terrain[k:(k+i),j:(j+i)] <- square.step(terrain[k:(k+i),j:(j+i)],sd)
+                }
+            }
+            sd <- sd * .75
+        }
+            return(terrain) 
+    }
+
+    terrain <- diamond.square.step(n,sd)
     if(water == TRUE)
-        terry[terry<0] <- NA
-    image(terry,col=terrain.colors(3))
+        terrain[terrain<0] <- NA
+    image(terrain,col=terrain.colors(3))
     dev.off()
+    return(terrain)
+
 }
-make.terrain.wrapper(6,20)
+
 
 # wrapper.terrain <- function(n, sd, water=TRUE){
     
